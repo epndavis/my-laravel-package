56cdbd956e309f93c6ca4b765e6d8ed53218823d
Elliot <elliot@huel.com>
2021-02-28 16:58:22 +0000

    Initial commit

---
+++ src/MyServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vendor\MyLaravelPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        
+    }
+}
